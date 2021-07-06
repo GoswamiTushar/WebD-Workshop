5ea9b74855213024ccc5034051172a03c3a4b857
GoswamiTushar <tushargoswami1510@gmail.com>
2021-07-06 20:35:08 +0530

    Projects section added

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ var menuButton = document.getElementById("menu-button");
 menuButton.addEventListener("click", function () {
   document.getElementById("nav-sec-2").classList.toggle("hide-item");
 });
+
+// projects section
+
+const projectsLinks = {
+  "Project-1": "#link1",
+  "Project-2": "#link2",
+  "Project-3": "#link3",
+};
+const projectDescription = {
+  "Project-1": "This is my project 1",
+  "Project-2": "This is my project 2",
+  "Project-3": "This is my project 3",
+};
+
+var projects = Object.keys(projectsLinks);
+var size = projects.length;
+var projectName = document.getElementById("project-name");
+var projectDesc = document.getElementById("project-desc");
+var projectLink = document.getElementById("project-link");
+var counter = 0;
+
+function changeData() {
+  if (counter === size) {
+    counter = 0;
+  } else if (counter < size) {
+    var project = projects[counter];
+    projectName.innerHTML = `${project}`;
+    projectDesc.innerHTML = `${projectDescription[project]}`;
+    projectLink.innerHTML = `${projectsLinks[project]}`;
+  }
+
+  counter += 1;
+  setTimeout(changeData, 3000);
+}
+
+changeData();
