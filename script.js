3d59641c18e55197fa9904b1aa1af372bb27b42e
GoswamiTushar <tushargoswami1510@gmail.com>
2021-07-06 19:54:24 +0530

    Nav-section addded

---
+++ script.js
@@ -0,0 +1,5 @@
+var menuButton = document.getElementById("menu-button");
+
+menuButton.addEventListener("click", function () {
+  document.getElementById("nav-sec-2").classList.toggle("hide-item");
+});
